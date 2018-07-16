e4712bf80b664a4e511a288bff2962732466c67b
Richard Westenra <rjwestenra@gmail.com>
2018-07-16 14:46:31 +0100

    Add lines

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Global constants
 const config = {
-  speed: 0.2,
-  apsis: 0.003
+  speed: 0.001,
+  apsis: 0.5,
+  lineInterval: 80
 };
 
 const planetData = [
@@ -15,45 +17,61 @@ const planetData = [
     color: 'red',
     radius: 5,
     start_angle: 0,
-    apsis: 57.9,
-    velocity: 1/58,
+    apsis: 0.387,
+    velocity: 1.59,
   },
   {
     color: 'lightyellow',
     radius: 12,
     start_angle: 0,
-    apsis: 108.2,
-    velocity: 1/243,
+    apsis: 0.723,
+    velocity: 1.18,
   },
 ];
 
+// Global mutable values
+let req,
+  width = window.innerWidth,
+  height = window.innerHeight,
+  center = {
+    x: width / 2,
+    y: height / 2
+  },
+  lines = [],
+  startTime = 0,
+  previousTime = 0,
+  timeSinceLastNewLine = 0;
+
+/**
+ * Create new Planet object, calculate its position, and draw it
+ */
 class Planet {
   constructor(props) {
     this.props = props;
     this.angle = this.props.start_angle;
   }
 
-  getAngle(time) {
+  updateAngle(time) {
     return this.angle += (this.props.velocity * time * config.speed);
   }
 
-  getPosition(angle) {
-    const center = {
-      x: width / 2,
-      y: height / 2
-    };
-    const apsisMultiplier = Math.min(width, height) * config.apsis;
-    const apsis = Math.round(this.props.apsis * apsisMultiplier);
-    return {
+  updatePosition(angle) {
+    const apsis = this.getApsis();
+    return this.position = {
       x: apsis * Math.cos(angle) + center.x,
       y: apsis * Math.sin(angle) + center.y
     };
   }
 
+  getApsis() {
+    const apsisMultiplier = Math.min(width, height) * config.apsis;
+    return this.apsis = Math.round(this.props.apsis * apsisMultiplier);
+  }
+
   draw(time) {
     const { context, radius, color } = this.props;
-    const angle = this.getAngle(time);
-    const { x, y } = this.getPosition(angle);
+    const angle = this.updateAngle(time);
+    const { x, y } = this.updatePosition(angle);
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
     context.strokeStyle = 'white';
@@ -62,7 +80,6 @@ class Planet {
     context.fill();
   }
 }
-
 
 /**
  * Create instances of the Area class
@@ -75,6 +92,54 @@ const makePlanets = () => {
       ...datum
     });
   });
+};
+
+/**
+ * Create new line between each planet
+ */
+class Line {
+  constructor(props) {
+    this.props = props;
+    this.coordinates = this.getCoordinates();
+  }
+
+  getCoordinates() {
+    return this.props.planets.map(d => {
+      const { x, y } = d.position;
+      return [x, y];
+    });
+  }
+
+  draw() {
+    const { context } = this.props;
+    context.beginPath();
+    context.moveTo(...this.coordinates[1]);
+    context.lineTo(...this.coordinates[2]);
+    context.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+    context.lineWidth = 2;
+    context.stroke();
+  }
+}
+
+const addNewLine = (timeSinceLastRun) => {
+  timeSinceLastNewLine += timeSinceLastRun;
+
+  while (timeSinceLastNewLine > config.lineInterval) {
+    timeSinceLastNewLine -= config.lineInterval;
+    lines.push(new Line({
+      planets,
+      context: m_context
+    }));
+  }
+};
+
+const drawPlanetPaths = planet => {
+    const { x, y } = center;
+    m_context.beginPath();
+    m_context.arc(x, y, planet.apsis, 0, 2 * Math.PI);
+    m_context.strokeStyle = 'rgba(255, 255, 255, 0.8)';
+    m_context.lineWidth = 2;
+    m_context.stroke();
 };
 
 /**
@@ -106,7 +171,10 @@ const clearCanvas = context => {
 const run = timestamp => {
   clearCanvas(m_context);
   const timeSinceLastRun = updateClock(timestamp);
+  addNewLine(timeSinceLastRun);
+  lines.forEach(d => d.draw());
   planets.forEach(d => d.draw(timeSinceLastRun));
+  planets.forEach(drawPlanetPaths);
   context.drawImage(m_canvas, 0, 0);
   req = requestAnimationFrame(run);
 };
@@ -120,6 +188,10 @@ const handleEvents = () => {
   window.addEventListener('resize', () => {
     width = window.innerWidth;
     height = window.innerHeight;
+    center = {
+      x: width / 2,
+      y: height / 2
+    };
     [canvas, m_canvas].forEach(updateCanvasSize);
   });
 
@@ -160,13 +232,6 @@ const initialise = () => {
   req = requestAnimationFrame(run);
   handleEvents();
 };
-
-// Establish some global mutable values
-let req,
-  width = window.innerWidth,
-  height = window.innerHeight,
-  startTime = 0,
-  previousTime = 0;
 
 // The canvas rendered to the page:
 const [canvas, context] = createCanvas();
